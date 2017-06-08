5acc3d5b1de7f314e6ae59bf00f1596ca3a2dfe6
Meredith Gregory <lgreg.meredith@biosimilarity.com>
2017-06-07 19:19:18 -0700

    Fleshed out REPL

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,14 +1,43 @@
+package com.akbarsgrasp.homesong
 object Main extends App {
   println("Hello, World!")
   @inline def defined(line: String) = {
      line != null && line.nonEmpty
   }
+  def processCmd( q : QuestionT ) : Unit = {
+    println( s"""should process q for ${q.songName}""" )
+  }
   def readUserQuery() : Unit = {
-   //Iterator.continually(scala.io.StdIn.readLine).takeWhile(defined(_)).foreach(c => println(c))
-   while(ok) {
-     val c = readChar()
-      ok = c != -1
-      if (ok) print(c)
-   }
- }
+    var l = List[Char]()
+    var ok = true        
+    // while(ok) {
+    //   val c = readChar()
+    //   ok = (c != -1)
+    //   if (ok) {
+    //     l = l ++ List[Char]( c )
+    //     print(c)
+    //   }
+    // }
+    println( "What do you want to get?" )
+    Iterator.continually(scala.io.StdIn.readLine).takeWhile(defined(_)).foreach(
+      {
+        ( l ) => {
+          println( s"""Ok, you want to get: ${l.toString}""" )
+          try { 
+            val cmd :: name :: Nil = l.split( " " ).toList
+            cmd match {
+              case "info" => processCmd( GetSongInfo( name ) )
+              case "motifs" => processCmd( GetMotifs( name ) )
+              case "composers" => processCmd( GetComposers( name ) )
+              case _ => throw new Exception( "" )
+            }
+          }
+          catch {
+            case e : Throwable => println( "ill-formated request" )
+          }
+          println( "What do you want to get?" )
+        }
+      }
+    )    
+  }
 }
--- src/main/scala/Questions.scala
+++ src/main/scala/Questions.scala
@@ -1,10 +1,21 @@
-//trait QuestionT {
-//}
+// -*- mode: Scala;-*- 
+// Filename:    Questions.scala 
+// Authors:     luciusmeredith                                                    
+// Creation:    Wed Jun  7 18:44:54 2017 
+// Copyright:   Not supplied 
+// Description: 
+// ------------------------------------------------------------------------
 
-//case class GetSongInfo( songName : String ) extends QuestionT
-//case class GetMotifs( songName : String ) extends QuestionT
-//case class GetComposers( songName : String ) extends QuestionT
-//case class GetBPM( songName : String ) extends QuestionT
-//case class GetLink( songName : String ) extends QuestionT
-//case class GetTimesignature( songName : String ) extends QuestionT
-//case object Quit extends QuestionT 
+package com.akbarsgrasp.homesong
+
+trait QuestionT {
+  def songName : String = ""
+}
+
+case class GetSongInfo( override val songName : String ) extends QuestionT
+case class GetMotifs( override val songName : String ) extends QuestionT
+case class GetComposers( override val songName : String ) extends QuestionT
+case class GetBPM( override val songName : String ) extends QuestionT
+case class GetLink( override val songName : String ) extends QuestionT
+case class GetTimesignature( override val songName : String ) extends QuestionT
+case object Quit extends QuestionT 
