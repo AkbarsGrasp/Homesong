d80f2579a3da8fbb1e356e894e02e60a05638bae
Meredith Gregory <lgreg.meredith@biosimilarity.com>
2017-06-28 18:56:36 -0700

    Small refactor

---
+++ src/main/scala/NamedSong.scala
@@ -0,0 +1,13 @@
+// -*- mode: Scala;-*- 
+// Filename:    NamedSong.scala 
+// Authors:     luciusmeredith                                                    
+// Creation:    Mon Jun 19 18:42:11 2017 
+// Copyright:   Not supplied 
+// Description: 
+// ------------------------------------------------------------------------
+
+package com.akbarsgrasp.homesong
+
+trait NamedSongT {
+  def songName : String = ""
+}
